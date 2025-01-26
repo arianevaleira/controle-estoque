a20ea48b604e4a6052f223d003b348c6521e15a4
Ariane valeria <153088012+arianevaleira@users.noreply.github.com>
2025-01-26 19:54:02 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,31 +1,31 @@
-create database db_estoque;
-use db_estoque;
+DROP DATABASE IF EXISTS db_estoque;
 
-create table tb_usuarios(
-usu_id int primary key auto_increment not null,
-usu_nome varchar(255),
-usu_senha varchar(255),
-usu_email varchar(255)
+CREATE DATABASE db_estoque;
+USE db_estoque;
+
+CREATE TABLE tb_clientes (
+    cli_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    cli_cnpj VARCHAR(18),
+    cli_nome VARCHAR(45)
 );
 
-create table tb_produtos(
-pro_id int primary key auto_increment not null,
-pro_nome varchar(255),
-pro_descricao varchar(255),
-pro_categoria varchar(45),
-pro_custo decimal(10,2),
-pro_preco decimal(10,2),
-pro_quantidade int(255)
+CREATE TABLE tb_produtos (
+    pro_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    pro_nome VARCHAR(255),
+    pro_descricao VARCHAR(255),
+    pro_categoria VARCHAR(45),
+    pro_custo DECIMAL(10, 2),
+    pro_preco DECIMAL(10, 2),
+    pro_quantidade INT
 );
 
-create table tb_movimentacoes(
-mov_id int primary key auto_increment not null,
-mov_pro_id int,
-mov_usu_id int,
-mov_motivo varchar(500),
-mov_data datetime,
-mov_quantidade int, # Quantidade que sera adicionada ou tirada do estoque 
-foreign key (mov_pro_id) references tb_produtos(pro_id),
-foreign key (mov_usu_id) references tb_usuarios(usu_id)
+CREATE TABLE tb_movimentacoes (
+    mov_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    mov_pro_id INT,
+    mov_cli_id INT,
+    mov_motivo VARCHAR(500),
+    mov_data DATETIME,
+    mov_quantidade INT,  -- Quantidade que será adicionada ou removida do estoque
+    FOREIGN KEY (mov_pro_id) REFERENCES tb_produtos(pro_id) ON DELETE CASCADE,
+    FOREIGN KEY (mov_cli_id) REFERENCES tb_clientes(cli_id)
 );
-
